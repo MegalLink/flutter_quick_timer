5ce7729c7e96735745364b5b6b0dcd9d9c62c04e
Jeferson Narváez <50426219+MegalLink@users.noreply.github.com>
2025-11-27 18:11:01 -0500

    Add license

--- lib/services/audio_service.dart
+++ lib/services/audio_service.dart
@@ -8,9 +8,35 @@ class AudioService {
 
   final AudioPlayer _player = AudioPlayer();
 
+  // Configurar el reproductor para no interrumpir otras apps
+  Future<void> _configurePlayer() async {
+    try {
+      // Configurar el modo de audio para que no pause otras apps
+      await _player.setAudioContext(
+        AudioContext(
+          iOS: AudioContextIOS(
+            category: AVAudioSessionCategory.ambient,
+            options: {AVAudioSessionOptions.mixWithOthers},
+          ),
+          android: AudioContextAndroid(
+            isSpeakerphoneOn: false,
+            stayAwake: false,
+            contentType: AndroidContentType.sonification,
+            usageType: AndroidUsageType.notification,
+            audioFocus: AndroidAudioFocus.gainTransientMayDuck,
+          ),
+        ),
+      );
+      await _player.setReleaseMode(ReleaseMode.stop);
+    } catch (e) {
+      debugPrint('Error configuring audio player: $e');
+    }
+  }
+
   // Reproducir sonido cuando termina el tiempo de trabajo
   Future<void> playWorkEndSound() async {
     try {
+      await _configurePlayer();
       await _player.play(AssetSource('sounds/end_bell.mp3'));
     } catch (e) {
       debugPrint('Error playing work end sound: $e');
@@ -20,6 +46,7 @@ class AudioService {
   // Reproducir sonido cuando termina el tiempo de descanso
   Future<void> playRestEndSound() async {
     try {
+      await _configurePlayer();
       await _player.play(AssetSource('sounds/start_bell.mp3'));
     } catch (e) {
       debugPrint('Error playing rest end sound: $e');
